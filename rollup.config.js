71af1f9b0b0be830c8606c374fa24cb7bad96637
Martin Quintana <martinquintana@ipsy.com>
2020-06-24 15:12:20 -0300

    Update files according to last prettier config

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import typescript from "rollup-plugin-typescript2";
-import external from "rollup-plugin-peer-deps-external";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
+import typescript from 'rollup-plugin-typescript2';
+import external from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
+      format: 'cjs',
+      exports: 'named',
       sourcemap: true,
     },
     {
       file: pkg.module,
-      format: "es",
-      exports: "named",
+      format: 'es',
+      exports: 'named',
       sourcemap: true,
     },
   ],
@@ -26,11 +26,11 @@ export default {
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
+      exclude: '**/__tests__/**',
       clean: true,
     }),
     commonjs({
-      include: ["node_modules/**"],
+      include: ['node_modules/**'],
     }),
   ],
 };
