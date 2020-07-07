e91557090a5e183ed17f30e767a4e435edff3b8d
Juan Pablo Vargas <jupavar@gmail.com>
2020-07-07 17:29:37 -0300

    chenge tsconfig

---
+++ build/components/Loading/index.d.ts
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+export declare type LoadingProps = {
+    loading?: boolean;
+    size?: number;
+    pink?: boolean;
+};
+declare const Loading: FC<LoadingProps>;
+export default Loading;
---
+++ build/lib/utils.d.ts
@@ -0,0 +1,2 @@
+export declare type Callback = () => void;
+export declare const noop: Callback;
