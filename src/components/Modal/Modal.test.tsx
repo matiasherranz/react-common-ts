7d0a5753674b90476eef484fd6abc8e43e000b6e
Matias Herranz <matias@ipsy.com>
2020-07-08 15:26:02 -0300

    Test updates.

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -6,18 +6,18 @@ import Modal from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Modal />, div);
+  ReactDOM.render(<Modal isOpen />, div);
 });
 
 it('doesnt render header by default', () => {
-  const component = shallow(<Modal />);
+  const component = shallow(<Modal isOpen />);
   expect(
     component.find('.modal .modal-header .icon-ipsy-close-cross').length
   ).toBe(0);
 });
 
 it('shows header when showDismiss', () => {
-  const component = shallow(<Modal showDismiss />);
+  const component = shallow(<Modal showDismiss isOpen />);
   expect(
     component.find('.modal .modal-header .icon-ipsy-close-cross').length
   ).toBe(1);
@@ -25,7 +25,7 @@ it('shows header when showDismiss', () => {
 
 it('should call onDismiss when the x on the header is clicked', () => {
   const onDismiss = jest.fn();
-  const component = shallow(<Modal showDismiss onDismiss={onDismiss} />);
+  const component = shallow(<Modal showDismiss isOpen onDismiss={onDismiss} />);
   component
     .find('.modal .modal-header .icon-ipsy-close-cross')
     .simulate('click');
